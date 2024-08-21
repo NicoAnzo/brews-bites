46d1115f5712178671fbbb3d87ecd69ff1b39bf1
Nicolas Anzoategui <nanzoate@gmail.com>
2024-08-21 17:36:40 +0200

    validate and redirect to notFoundPage for cases where the productId doesn't exist

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,8 +1,11 @@
 function NotFoundPage () {
     
     return (
-        <div className="not-found-container">
-            <p>Not Found Page</p>
+        <div className="not-found-wrapper">
+            <div className="not-found-page-container">
+                <h3>Oops! Page Not Found</h3>
+                <p>The page you are looking for doesn't exist or has been moved.</p>
+            </div>
         </div>
     );
 }
--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,27 +1,33 @@
 import { useState, useEffect } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import { API_URL } from "../config/api";
+
 import axios from "axios";
+import NotFoundPage from "./NotFoundPage";
 
 function ProductDetailsPage () {
   
     const [product, setProduct] = useState(null);
+    const [error, setError] = useState(false);
     const { productId } = useParams();
     const navigate = useNavigate();
 
     const getProduct = () => {
-        axios
-            .get(`${API_URL}/products/${productId}`)
+        axios.get(`${API_URL}/products/${productId}`)
             .then((response) => {
                 const singleProduct = response.data;
-                setProduct(singleProduct);
+                if (singleProduct) {
+                    setProduct(singleProduct);
+                } else {
+                    setError(true);
+                }
             })
-            .catch((error) => console.log(error));
-    };
+            .catch(() => setError(true));
+    };       
 
     useEffect(() => {
         getProduct();
-    }, []);
+    }, [productId]);
 
     const deleteProduct = () => {
         axios.delete(`${API_URL}/products/${productId}`)
@@ -31,28 +37,33 @@ function ProductDetailsPage () {
             .catch((err) => console.log(err));
     };
 
-    return (
-        <div className="product-details-container">
-            <div className="details-card-container">
-            {product && (
-                <>
-                    <h1>{product.name}</h1>
-                    <img src={product.image} alt="product image" />
-                    <p><strong>Description:</strong> {product.description}</p>
-                    <p><strong>Stock:</strong> {product.quantity} servings</p>
-                    <p><strong>Price:</strong> {product.price} €</p>
-                    <p><strong>Category:</strong> {product.category}</p>
-                </>
-                )}
+    if (error) {
+        return <NotFoundPage />;
+    }   
 
-                <div className="button-group">
-                    <Link to={`/products/edit/${productId}`}>
-                    <button className="btn-edit">Edit Product</button>
-                    </Link>
-                    <button className="btn-delete" onClick={deleteProduct}>Delete Product</button>
+    return (
+        <>
+            {product && (
+                
+                <div className="product-details-container">
+                    <div className="details-card-container">
+                        <h1>{product.name}</h1>
+                        <img src={product.image} alt="product image" />
+                        <p><strong>Description:</strong> {product.description}</p>
+                        <p><strong>Stock:</strong> {product.quantity} servings</p>
+                        <p><strong>Price:</strong> {product.price} €</p>
+                        <p><strong>Category:</strong> {product.category}</p>
+
+                        <div className="button-group">
+                            <Link to={`/products/edit/${productId}`}>
+                            <button className="btn-edit">Edit Product</button>
+                            </Link>
+                            <button className="btn-delete" onClick={deleteProduct}>Delete Product</button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            )}
+        </>  
     );
 }
 
